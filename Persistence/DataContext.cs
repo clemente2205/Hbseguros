f113f9bf18c4d52c0bc29cf27270260fff49df2a
clemente <peprg2@gmail.com>
2022-09-18 23:30:29 -0500

    proyecto nuevo

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    internal class DataContext : DbContext
+    {
+        #region Singleton
+        private DataContext() { }
+
+        private static DataContext instance = null;
+        public static DataContext Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataContext();
+                }
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+        #region Methods
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+
+        #endregion Methods
+    }
+}
